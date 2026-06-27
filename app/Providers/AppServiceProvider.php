1bce303c288596998e091ef79e640a9be3895495
adrianKiv <adrianmulianto@gmail.com>
2026-06-28 00:12:32 +0700

    migration  update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Paginator::useBootstrapFive();
 
         Event::listen(Login::class, AttachUserActivitiesToLogin::class);
+
     }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('id_role')->default(1); // 2 for regular user, 1 for admin
             $table->rememberToken();
             $table->timestamps();
         });
