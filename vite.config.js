727c5e3576f440de518883195af3439f74d4aaec
adrianKiv <adrianmulianto@gmail.com>
2026-06-28 01:03:50 +0700

    register refactor assets to vite config

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,18 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/css/landing.css',
                 'resources/css/map.css',
+                'resources/css/refactor.css',
+                'resources/css/admin/dashboard.css',
+
                 'resources/js/app.js',
                 'resources/js/landing.js',
                 'resources/js/map.js',
-                'resources/js/location-picker.js'
+                'resources/js/location-picker.js',
+                'resources/js/refactor/umkm-submission-modal.js',
+                'resources/js/refactor/admin-charts.js',
+                'resources/js/refactor/admin-lokasi-map.js',
+                'resources/js/refactor/admin-menu-list.js',
+                'resources/js/refactor/map-modals.js'
             ],
             refresh: true,
         }),
