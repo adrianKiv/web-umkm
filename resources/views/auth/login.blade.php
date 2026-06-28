3b1b4c6a3dfea9c3b5b157b0c8f2be5bc3700fff
adrianKiv <adrianmulianto@gmail.com>
2026-06-28 22:05:17 +0700

    login bug fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -273,12 +273,13 @@
 
             .auth-panel {
                 border-radius: 24px 24px 0 0;
-                order: 2;
+                order: 1;
             }
 
             .auth-form {
                 padding: 2rem;
-                order: 1;
+                border-radius: 0 0 24px 24px;
+                order: 2;
             }
         }
 
